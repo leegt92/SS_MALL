39002184590f079e24b866e3dd742710a42d0193
leegt92 <leegt92@gmail.com>
2020-02-07 17:29:08 +0900

    product와 image join에 사용하는 ProductImageVO를 추가했음. product 테이블과 image테이블을 조인해 결과값을 출력했다.

--- src/main/java/edu/bit/ssmall/mapper/ProductMapper.java
+++ src/main/java/edu/bit/ssmall/mapper/ProductMapper.java
@@ -4,12 +4,12 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Select;
 
-import edu.bit.ssmall.productVO.ProductVO;
+import edu.bit.ssmall.productVO.ProductImageVO;
 
 public interface ProductMapper {
 	
 
 	@Select("select * from product p , image i where p.p_number = i.p_number order by p_name desc")
-	public List<ProductVO> selectProductList();		
+	public List<ProductImageVO> selectProductList();		
 
 }
